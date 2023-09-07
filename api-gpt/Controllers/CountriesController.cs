b3dbdb53ebc10bbd84cd2ef5b2187f5556c5130e
MatrixCodeK <matrixcodek@gmail.com>
2023-09-06 22:31:30 -0500

    UC#1 Create endpoint

--- api-gpt/Controllers/CountriesController.cs
+++ api-gpt/Controllers/CountriesController.cs
@@ -1,22 +1,27 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 
 [Route("api/[controller]")]
 [ApiController]
 public class CountriesController : ControllerBase
 {
     private readonly HttpClient _httpClient;
+    private readonly IConfiguration _configuration;
 
-    public CountriesController(IHttpClientFactory httpClientFactory)
+    public CountriesController(IHttpClientFactory httpClientFactory, IConfiguration configuration)
     {
         _httpClient = httpClientFactory.CreateClient();
+        _configuration = configuration;
     }
 
-    [HttpGet("all")]
-    public async Task<IActionResult> GetAllCountries()
+    [HttpGet("")]
+    public async Task<IActionResult> GetAllCountries(string? param1 = null, int? param2 = null, string? param3 = null, string? param4 = null)
     {
-        var response = await _httpClient.GetAsync("https://restcountries.com/v3.1/all");
+        var URL = _configuration.GetValue<string>("RestCountriesURL");
+        Console.WriteLine($"Vamos a ver la URL: {URL}");
+        var response = await _httpClient.GetAsync(URL);
 
         if (response.IsSuccessStatusCode)
         {
