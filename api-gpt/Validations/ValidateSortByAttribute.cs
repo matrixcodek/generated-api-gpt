56ab3ac678a8a4d79a9396a75db368ac92ed1474
MatrixCodeK <matrixcodek@gmail.com>
2023-09-12 11:04:24 -0500

    UC#1 Create pagination function

--- api-gpt/Validations/ValidateSortByAttribute.cs
+++ api-gpt/Validations/ValidateSortByAttribute.cs
@@ -6,13 +6,23 @@ namespace api_gpt.Validations
         AllowMultiple = false)]
     public class ValidateSortByAttribute : ValidationAttribute
     {
+
+        private const string AscendValue = "ascend";
+        private const string DescendValue = "descend";
+        private const string ErrorMessageForUser = "The sortBy filter must be either 'ascend' or 'descend' only";
         protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
         {
-            if(string.IsNullOrEmpty((string?)value) || value?.ToString()?.ToLower() == "ascend" || value?.ToString()?.ToLower() == "descend")
+            if(value is string stringValue && IsSortByValid(stringValue))
             {
                 return ValidationResult.Success;
             }
-            return new ValidationResult("The sortBy filter must be either 'ascend' or 'descend' only");
+            return new ValidationResult(ErrorMessageForUser);
+        }
+
+        private static bool IsSortByValid(string sortBy)
+        {
+            sortBy = sortBy.ToLower();
+            return string.IsNullOrEmpty(sortBy) || sortBy.Equals(AscendValue) || sortBy.Equals(DescendValue);
         }
     }
 }
